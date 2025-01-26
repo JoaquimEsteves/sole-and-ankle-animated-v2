2d97386283aa45a7f78b99c596243786726d404e
Joaquim Esteves <joaquimbve@hotmail.com>
2025-01-26 17:23:48 +0100

    ✨ Part two done!

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 import { QUERIES, WEIGHTS } from "../../constants";
 import Logo from "../Logo";
@@ -51,6 +51,13 @@ const Header = () => {
     </header>
   );
 };
+
+const NavLink = React.memo((props) => (
+  <LinkWrapper>
+    <NavLinkNormal {...props} />
+    <NavLinkFancy {...props} />
+  </LinkWrapper>
+));
 
 const MainHeader = styled.div`
   display: flex;
@@ -114,15 +121,56 @@ const Filler = styled.div`
   }
 `;
 
-const NavLink = styled.a`
+const LinkWrapper = styled.span`
+  position: relative;
+`;
+
+// TODO: Add props to CSS so I don't have som uch repetition
+// Who cares
+const common = css`
+  ${LinkWrapper}:first-of-type & {
+    color: var(--color-secondary);
+  }
+
+  display: inline-block;
+  transition:
+    transform 200ms,
+    opacity 200ms;
+
   font-size: 1.125rem;
   text-transform: uppercase;
   text-decoration: none;
   color: var(--color-gray-900);
+`;
+
+const NavLinkNormal = styled.a`
+  ${common}
   font-weight: ${WEIGHTS.medium};
 
-  &:first-of-type {
-    color: var(--color-secondary);
+  transform: translateY(0);
+  opacity: 1;
+
+  ${LinkWrapper}:hover & {
+    transform: translateY(-100%);
+    opacity: 0;
+  }
+`;
+
+const NavLinkFancy = styled.a`
+  ${common}
+
+
+  left: 0;
+  position: absolute;
+  font-weight: ${WEIGHTS.bold};
+
+  transform: translateY(+100%);
+  opacity: 0;
+
+
+  ${LinkWrapper}:hover & {
+    transform: translateY(0%);
+    opacity: 1;
   }
 `;
 
